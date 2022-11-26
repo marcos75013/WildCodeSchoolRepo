e8d31eb3d6eb95ef58c8d7ed524c93b9130131d6
marcos75013 <marcosmartinsfr@gmail.com>
2022-11-26 14:47:18 +0100

    dernieres chgts site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-let message = "The kingdom of colchis, thanks you for your interest, we will get back to you as soon as possible.";
-
+let message = "message received successfully. We will contact you quickly";
 
 document.getElementById('formContact')
         .addEventListener("submit", function (e) {
