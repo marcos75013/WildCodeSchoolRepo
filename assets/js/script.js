a4882538ae435d827d0cebc2ebb4c998e23f2538
marcos75013 <marcosmartinsfr@gmail.com>
2022-11-27 15:54:23 +0100

    add gitignore

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ let message = "message received successfully. We will contact you quickly";
 
 document.getElementById('formContact')
         .addEventListener("submit", function (e) {
+            console.log(e);
             e.preventDefault();
             alert(message)
         });
